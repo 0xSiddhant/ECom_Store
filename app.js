6c7428e399cbf364627ccbd26e533f39e9c9e2d2
Siddhant2155 <contactsiddhant2155@gmail.com>
2021-12-12 16:22:41 +0530

    Logger, Swagger, ... configurated

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require("express")
 require("dotenv").config()
 const app = express()
+const morgan = require('morgan')
+const cookieParser = require("cookie-parser")
+const fileUpload = require("express-fileupload")
+
+// morgan middleware
+app.use(morgan('tiny'))
+
+//for swagger documentation
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDoc = YAML.load('./swagger.yaml')
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc))
+
+//regualar middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// cookies and file middleware
+app.use(fileUpload())
+app.use(cookieParser())
 
 // import routes
 const home = require("./routes/home")
 
 // routes middleware
 app.use("/api/v1", home);
-
 
 
 // export app js
