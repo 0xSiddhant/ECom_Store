6c72f8910e260478c6466a38ff837cbf4f242f64
Siddhant2155 <contactsiddhant2155@gmail.com>
2021-12-19 20:30:35 +0530

    signup logic implemented

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,33 @@
 const User = require("../models/user")
 const BigPromise = require("../middlewares/bigPromise")
+const CustomError = require("../utils/customError")
 
 exports.signup = BigPromise( async (req, res, next) => {
-    res.send("Signup route")
+
+    const {name, email, password} = req.body
+
+    if (!email || !name || !password) {
+        return next(new CustomError("Name, Email and Password are required", 400))
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    const token = user.getJWTToken()
+
+    const options = {
+        expires: new Date(
+            Date.now() + 3 * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+
+    res.status(200).cookie('token', token, options).json({
+        success: true,
+        token,
+        user
+    })
 })
